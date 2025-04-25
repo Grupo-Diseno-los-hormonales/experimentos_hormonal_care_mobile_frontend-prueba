0730e1059112eb8e0dd3fca509d96def2167b95b
JHS-Sama24 <112771173+JHS-Sama24@users.noreply.github.com>
2025-04-25 11:05:20 -0500

    feat: update patient list

--- lib/scr/features/appointment/presentation/pages/patients_list_screen.dart
+++ lib/scr/features/appointment/presentation/pages/patients_list_screen.dart
@@ -7,7 +7,6 @@ import 'package:experimentos_hormonal_care_mobile_frontend/scr/features/appointm
 import 'package:experimentos_hormonal_care_mobile_frontend/scr/core/utils/usecases/jwt_storage.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
-import 'package:url_launcher/url_launcher.dart';
 
 class HomePatientsScreen extends StatefulWidget {
   final int doctorId;
@@ -30,6 +29,7 @@ class _HomePatientsScreenState extends State<HomePatientsScreen> {
   @override
   void initState() {
     super.initState();
+    tz.initializeTimeZones();
     _fetchPatients();
   }
 
@@ -39,7 +39,6 @@ class _HomePatientsScreenState extends State<HomePatientsScreen> {
     });
 
     try {
-      final userId = await JwtStorage.getUserId();
       final role = await JwtStorage.getRole();
 
       if (role != 'ROLE_DOCTOR') {
@@ -57,17 +56,16 @@ class _HomePatientsScreenState extends State<HomePatientsScreen> {
           'name': profileDetails['fullName'] ?? 'No name',
           'time': appointment['startTime'] ?? 'No start time',
           'endTime': appointment['endTime'] ?? 'No end time',
-          'image': profileDetails['image'] ?? '', // Assuming 'image' is the key for the profile image URL
+          'image': profileDetails['image'] ?? '',
           'eventDate': appointment['eventDate'] ?? 'No date',
           'patientId': appointment['patientId'].toString(),
           'title': appointment['title'] ?? 'No title',
           'description': appointment['description'] ?? 'No description',
-          'color': appointment['color'] ?? '0xFF039BE5', // Default color if none is provided
-          'appointmentId': appointment['id'].toString(), // Add appointment ID
+          'color': appointment['color'] ?? '0xFF039BE5',
+          'appointmentId': appointment['id'].toString(),
         });
       }
 
-      // Ordenar las citas por hora
       fetchedPatients.sort((a, b) {
         final aTime = tz.TZDateTime.from(DateTime.parse(a['eventDate']!), limaTimeZone);
         final bTime = tz.TZDateTime.from(DateTime.parse(b['eventDate']!), limaTimeZone);
@@ -115,81 +113,34 @@ class _HomePatientsScreenState extends State<HomePatientsScreen> {
                       final eventDate = tz.TZDateTime.from(DateTime.parse(patients[index]['eventDate']!), limaTimeZone);
                       final isPast = eventDate.isBefore(now);
 
+                      // Agregar fecha actual a las horas para evitar errores de formato
+                      final today = DateTime.now();
+                      final startTime = DateTime.parse("${today.toIso8601String().split('T')[0]} ${patients[index]['time']!}");
+                      final endTime = DateTime.parse("${today.toIso8601String().split('T')[0]} ${patients[index]['endTime']!}");
+
+                      final formattedStartTime = "${startTime.hour % 12 == 0 ? 12 : startTime.hour % 12}:${startTime.minute.toString().padLeft(2, '0')} ${startTime.hour >= 12 ? 'PM' : 'AM'}";
+                      final formattedEndTime = "${endTime.hour % 12 == 0 ? 12 : endTime.hour % 12}:${endTime.minute.toString().padLeft(2, '0')} ${endTime.hour >= 12 ? 'PM' : 'AM'}";
+
                       return Card(
-                        color: isPast ? Color(0xFFB0BEC5) : Color(0xFFE0E0E0), // Oscurecer las citas pasadas
+                        color: isPast ? Color(0xFFB0BEC5) : Color(0xFFD1C4E9), // Cambiar color de fondo
                         margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                        child: Stack(
-                          children: [
-                            ListTile(
-                              leading: CircleAvatar(
-                                backgroundImage: NetworkImage(patients[index]['image']!),
-                                backgroundColor: Color(0xFF6A828D),
-                              ),
-                              title: Text(
-                                patients[index]['name']!,
-                                style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
-                              ),
-                              trailing: Padding(
-                                padding: EdgeInsets.only(right: 16), // Move the container to the left
-                                child: GestureDetector(
-                                  onTap: () async {
-                                    final url = patients[index]['description']!;
-                                    if (await canLaunch(url)) {
-                                      await launch(url);
-                                    } else {
-                                      throw 'Could not launch $url';
-                                    }
-                                  },
-                                  child: Container(
-                                    padding: EdgeInsets.symmetric(horizontal: 4, vertical: 4), // Adjusted padding
-                                    decoration: BoxDecoration(
-                                      color: Color(0xFF40535B),
-                                      borderRadius: BorderRadius.circular(8),
-                                    ),
-                                    child: Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        Icon(Icons.videocam, color: Colors.white),
-                                        SizedBox(width: 4), // Adjusted spacing
-                                        Text(
-                                          '${patients[index]['time']} - ${patients[index]['endTime']}',
-                                          style: TextStyle(color: Colors.white),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Positioned(
-                              right: 8,
-                              top: 0,
-                              bottom: 0,
-                              child: Align(
-                                alignment: Alignment.center,
-                                child: CircleAvatar(
-                                  radius: 12, // Half the size of the original
-                                  backgroundColor: Color(0xFF40535B),
-                                  child: Center(
-                                    child: IconButton(
-                                      padding: EdgeInsets.zero,
-                                      icon: Icon(Icons.info, color: Colors.white, size: 16),
-                                      onPressed: () {
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                            builder: (context) => AppointmentDetail(
-                                              appointmentId: int.parse(patients[index]['appointmentId']!),
-                                            ),
-                                          ),
-                                        );
-                                      },
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
+                        child: ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(patients[index]['image']!),
+                            backgroundColor: Color(0xFF9575CD), // Cambiar color del avatar
+                          ),
+                          title: Text(
+                            patients[index]['name']!,
+                            style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xFF4A148C)), // Cambiar color del texto
+                          ),
+                          subtitle: Text(
+                            "${patients[index]['age']} years old",
+                            style: TextStyle(color: Color(0xFF6A1B9A)),
+                          ),
+                          trailing: Text(
+                            "$formattedStartTime - $formattedEndTime",
+                            style: TextStyle(color: Color(0xFF4A148C), fontWeight: FontWeight.bold), // Mostrar hora inicio y fin
+                          ),
                         ),
                       );
                     },
@@ -208,7 +159,7 @@ class _HomePatientsScreenState extends State<HomePatientsScreen> {
           );
 
           if (result == true) {
-            _fetchPatients(); // Refresca la pantalla si se ha creado una cita
+            _fetchPatients();
           }
         },
         child: Icon(Icons.add),
